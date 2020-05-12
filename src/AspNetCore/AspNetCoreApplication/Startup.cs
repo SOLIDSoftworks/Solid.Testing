82429caeff30c4bdf7bc0d7b0399db34394f367c
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-05-12 09:26:41 +0000

    Update Startup.cs

--- src/AspNetCore/AspNetCoreApplication/Startup.cs
+++ src/AspNetCore/AspNetCoreApplication/Startup.cs
@@ -20,7 +20,7 @@ namespace AspNetCoreApplication
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.Use(async (context, next) =>
             {
